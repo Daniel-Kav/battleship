9d1d88f05bb0d8d6d8d945ace4b84579b0b0bc8a
daniel-kav <dankavatha01@gmail.com>
2023-09-25 22:23:28 +0300

    adding ship to the gaming board

--- src/drag.js
+++ src/drag.js
@@ -1,9 +1,26 @@
 //drag player ships
 const optionContainer = document.querySelector('.option-container');
+let draggedShip;
+const ships = [destroyer, submarine, cruiser, battleship, carrier]
 
 
 const optionShips = Array.from(optionContainer.children)
 optionShips.forEach(optionShip => optionShip.addEventListener('dragstart', dragstart));
-export function dragstart(e){
-    console.log(e.target);
- }
+const allPlayerBlocks =  document.querySelectorAll('#player div');
+
+allPlayerBlocks.forEach(playerBlock => {
+    playerBlock.addEventListener('dragover', dragover);
+    playerBlock.addEventListener('drop', dropShip);
+}
+
+function dragstart(e){
+draggedShip = e.target;
+}
+
+function dragover(e){
+    e.preventDefault();
+}
+function dropShip(){
+    const startId = e.target.id;
+    const ship = ships[draggedShip.id];  
+}
